b07a551264263ff524616031cd2d4c807147fb14
SamreenM15 <72513985+SamreenM15@users.noreply.github.com>
2020-11-13 18:28:24 -0500

    Add files via upload

---
+++ buttons.js
@@ -0,0 +1,59 @@
+//BUTTONS ASSIGNMENT 
+
+function sayHello() {
+    alert("Hello there! My name is Samreen!");
+}
+function sayGoodbye() {
+alert ("See ya! Thanks for visiting my website!");
+}
+
+
+//mini question buttons
+function question1(){
+    let name = prompt("Hi! Nice to meet you! What's your name?");
+    alert(name + "? Awesome name! Great to see you, " + name + "!");
+}
+
+function question2() {
+    let sport = prompt(" What's your favourite sport?");
+    alert("Nice! That's a really fun sport to play!"); 
+}
+
+function question3() {
+    let food = prompt("Final question! What's your favourite food to eat?");
+    alert(food + " is delicious!");
+}
+
+
+
+//SOME MATH 
+function varDemo() {
+    alert("check the console and answer some addition questions!")
+    let firstnumber = 15;
+    let secondnumber = 8;
+    let answer = firstnumber + secondnumber;
+    console.log(answer + "Nice work! Let's keep going!");
+}
+
+function varDemo2() {
+    alert("check the console and answer some divison questions!")
+    let firstnumber = 36
+    let secondnumber = 12
+    let answer = firstnumber / secondnumber
+    console.log(answer + "Hope you're having fun!")
+}
+
+function varDemo3() {
+    alert("check the console and answer some subtraction questions!")
+    let firstnumber = 100
+    let secondnumber = 37
+    let answer = firstnumber * secondnumber
+    console.log(answer + "You're done! You're a math superstar!")
+}
+
+//random button
+function midway() {
+    alert("Almost there!")
+}
+
+  
